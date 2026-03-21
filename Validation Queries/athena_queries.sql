809003115730cd7b16feb0ef51f119fffafc3675
student <aguz174@wgu.edu>
2026-03-21 08:46:18 +0000

    Update Variable Names

--- Validation Queries/athena_queries.sql
+++ Validation Queries/athena_queries.sql
@@ -4,10 +4,10 @@ SELECT COUNT(*) as accelerometer_count FROM accelerometer_landing;
 SELECT COUNT(*) as step_trainer_count FROM steptrainer_landing;
 
 -- Trusted Zones Validation Checks
-SELECT COUNT(*) as consenting_customers FROM customer_trusted;
+SELECT COUNT(*) as consent_customers FROM customer_trusted;
 SELECT COUNT(*) as trusted_accel FROM accelerometer_trusted;
 SELECT COUNT(*) as trusted_step FROM steptrainer_trusted;
 
 -- Curated Zones Validation Checks
 SELECT COUNT(*) AS customer_curated_count FROM customer_curated;
-SELECT COUNT(*) AS machine_learning_curated_count FROM machine_learning_curated;
+SELECT COUNT(*) AS machinelearning_curated_count FROM machinelearning_curated;
