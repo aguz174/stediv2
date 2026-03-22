a86642059f013c1ec40792c9b7441fb25f3ece57
student <aguz174@wgu.edu>
2026-03-22 05:19:34 +0000

    Revisions

--- Validation Queries/athena_queries.sql
+++ Validation Queries/athena_queries.sql
@@ -3,6 +3,8 @@ SELECT COUNT(*) as customer_count FROM customer_landing;
 SELECT COUNT(*) as accelerometer_count FROM accelerometer_landing;
 SELECT COUNT(*) as step_trainer_count FROM steptrainer_landing;
 
+SELECT COUNT(*) FROM stedi-2.customer_landing WHERE shareWithResearchAsOfDate IS NULL
+
 -- Trusted Zones Validation Checks
 SELECT COUNT(*) as consent_customers FROM customer_trusted;
 SELECT COUNT(*) as trusted_accel FROM accelerometer_trusted;
